b72b07baaaaabeb7d31b0bb68f0d1b7890f9d69b
Ilham Oktavian <ilham.oktavian1313@gmail.com>
2021-01-11 11:53:30 +0700

    first commit

---
+++ home.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class home : MonoBehaviour
+{
+    public GameObject ImageTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void Button()
+    {
+        SceneManager.LoadScene("UI");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
